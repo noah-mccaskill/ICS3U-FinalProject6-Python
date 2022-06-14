471f13408cae71a45dc2e8e9e2b8059874134f98
noah-mccaskill <94465036+noah-mccaskill@users.noreply.github.com>
2022-06-14 10:48:11 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Created by: Noah McCaskill
+# Created on: June 2022
+# This constants file is for the Space Aliens game
+
+# PyBadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10 
+SCREEN_GRID_Y = 8 
+SPRITE_SIZE = 16 
+TOTAL_NUMBER_OF_ALIENS = 5 
+FPS = 60 
+SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state 
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
